f457e85f7a72238b8da3a68e5a5390889aedaf00
Ste <stephenkelehan@gmail.com>
2019-04-19 18:24:54 +0100

    Init JS

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("This is a test")
+
+// init vars
+const userScore = 0;
+const computerScore = 0;
+// Links it to the html id
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const rock_div = document.getElementById("r");
+const sissors_div = document.getElementById("s");
+const paper_div = document.getElementById("p");
+// Links it to the html class
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+
+
+
+
+
+
+
